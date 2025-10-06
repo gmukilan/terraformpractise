8a25e1f7680143de2a41dba3c5b3d3a53ce047b1
gmukilan <mukilanvdu@gmail.com>
2025-10-06 16:34:24 +0530

    add new storage account

--- main.tf
+++ main.tf
@@ -3,3 +3,11 @@ resource "azurerm_resource_group" "myrg" {
   name     = "iyapparg"
   location = "East US"
 }
+
+resource "azurerm_storage_account" "mystorageaccount" {
+  name                     = "iyappastorageaccout"
+  resource_group_name      = azurerm_resource_group.myrg.name
+  location                 = azurerm_resource_group.myrg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
