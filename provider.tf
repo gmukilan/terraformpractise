855bd8f7596952f1686cfd3fb6a231dbded5f785
gmukilan <155289014+gmukilan@users.noreply.github.com>
2025-10-06 12:43:47 +0530

    created provider file to initiate commit

---
+++ provider.tf
@@ -0,0 +1,22 @@
+# provider.tf
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+
+  required_version = ">= 1.0"
+}
+
+provider "azurerm" {
+  features {}
+
+  # These values can be set via environment variables or GitHub Secrets
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+}
