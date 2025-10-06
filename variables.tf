3536cc54caaa3bd2cfe03f0cf89689790ed5441a
gmukilan <mukilanvdu@gmail.com>
2025-10-06 15:51:39 +0530

    my first commit iyappa Jeeva

--- variables.tf
+++ variables.tf
@@ -1,4 +1,30 @@
-variable "subscription_id" {}
-variable "client_id" {}
-variable "client_secret" {}
-variable "tenant_id" {}
+variable "client_id" {
+    description = "The Client ID of the Service Principal."
+    type = string
+  
+}
+
+
+variable "subscription_id" {
+    description = "The Client ID of the Service Principal."
+    type = string
+    default = "b4e43cd8-0b46-43b3-8ea3-b4f5afd4dcab"
+  }
+
+variable "client_id" {
+    description = "The Client ID of the Service Principal."
+    type = string
+    default = "7fd92cfc-6490-41c9-a389-9f4daf254f99"
+    }
+
+variable "client_secret" {
+    description = "The Client ID of the Service Principal."
+    type = string
+    default = "0be23bdd-6352-4607-a9ce-d81e8e66c14a"
+  }
+  
+variable "tenant_id" {
+    description = "The Client ID of the Service Principal."
+    type = string
+default = "3c088a68-7326-4535-b6c1-c1406a5f35d5"
+  }
