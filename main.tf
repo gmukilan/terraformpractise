b1698c699738c26992583c9bfc1214ff23f1656e
gmukilan <mukilanvdu@gmail.com>
2025-10-07 16:43:59 +0530

    Deleted storage account

--- main.tf
+++ main.tf
@@ -4,10 +4,11 @@ resource "azurerm_resource_group" "myrg" {
   location = "East US"
 }
 
-resource "azurerm_storage_account" "mystorageaccount" {
-  name                     = "iyappastorageaccout"
-  resource_group_name      = azurerm_resource_group.myrg.name
-  location                 = azurerm_resource_group.myrg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
+#resource "azurerm_storage_account" "mystorageaccount" {
+ # name                     = "iyappastorageaccout"
+  #resource_group_name      = azurerm_resource_group.myrg.name
+  #location                 = azurerm_resource_group.myrg.location
+  #account_tier             = "Standard"
+  #account_replication_type = "LRS"
+#}
+
--- variables.tf
+++ variables.tf
@@ -1,13 +1,11 @@
 variable "subscription_id" {
     description = "The Client ID of the Service Principal."
     type = string
-    default = "b4e43cd8-0b46-43b3-8ea3-b4f5afd4dcab"
   }
 
 variable "client_id" {
     description = "The Client ID of the Service Principal."
     type = string
-    default = "7fd92cfc-6490-41c9-a389-9f4daf254f99"
     }
 
 variable "secret_value" {
@@ -18,5 +16,4 @@ variable "secret_value" {
 variable "tenant_id" {
     description = "The Client ID of the Service Principal."
     type = string
-default = "3c088a68-7326-4535-b6c1-c1406a5f35d5"
   }
