ef186e7f90dd66884986fbaa7ab5d1334ba4e8f4
gmukilan <mukilanvdu@gmail.com>
2025-10-27 16:50:42 +0530

    aki

--- storagemodule.tf
+++ storagemodule.tf
@@ -1,6 +1,6 @@
 resource "azurerm_storage_account" "mukakstorage" {
   name                     = var.storage_account_name
-  resource_group_name      = module.namerg.azurerm_resource_group_name
+  resource_group_name      = module.namerg.azurerm_resource_group_name.name
   location                 = var.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
